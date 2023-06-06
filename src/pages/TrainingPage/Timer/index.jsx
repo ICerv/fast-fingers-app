731c167bef61170c0de15d2eeb8697b7b4f92802
Inna Cervenkova <inna.cervenkova@gmail.com>
2023-06-07 00:08:58 +0200

    Add stop/start functionality and auto-reset for Timer

--- src/pages/TrainingPage/Timer/index.jsx
+++ src/pages/TrainingPage/Timer/index.jsx
@@ -2,36 +2,67 @@ import React, { useState, useEffect } from 'react';
 import './style.css';
 
 export const Timer = ({ onStart, onEnd }) => {
-  const [time, setTime] = useState(60); // start time
-  const [isStarted, setIsStarted] = useState(false); // track if timer is started
+  const [time, setTime] = useState(5);
+  const [isStarted, setIsStarted] = useState(false);
 
   useEffect(() => {
     let timer;
     if (isStarted && time > 0) {
       timer = setInterval(() => {
-        setTime(time - 1);
+        setTime(prevTime => prevTime - 1);
       }, 1000);
-    } else if (!isStarted) {
+    } else if (!isStarted && time !== 5) {
       clearInterval(timer);
-      setTime(60); // reset time when stopped
-    } else {
-      clearInterval(timer);
-      onEnd();
+      //   setTime(5);
+      //   onEnd();
+      //   setIsStarted(false);
+      // } else if (!isStarted) {
+      //   clearInterval(timer);
+      //   // } else {
+      //   //   clearInterval(timer);
+      //   //   onEnd();
     }
     return () => {
       clearInterval(timer);
     };
   }, [isStarted, time]);
 
-  const handleStart = () => {
-    setIsStarted(true);
-    onStart();
+  useEffect(() => {
+    let resetTimer;
+    if (time === 0) {
+      resetTimer = setTimeout(() => {
+        setTime(5);
+        setIsStarted(false);
+      }, 2000);
+    }
+    return () => {
+      clearTimeout(resetTimer);
+    };
+  }, [time]);
+
+
+
+  const handleStopStart = () => {
+    setIsStarted(prevIsStarted => !prevIsStarted);
+    // setIsStarted(!isStarted);
+    if (!isStarted && time !== 0) {
+      onStart();
+    } else {
+      onEnd();
+    }
   };
+
+  const buttonClass = isStarted ? ' btn timer-stop-btn' : 'btn timer-start-btn';
+
 
   return (
     <div className='timer-container'>
       {/* START TIMER BUTTON */}
-      <button className='timer-start-btn' onClick={handleStart}>Start</button>
+      <button
+        className={buttonClass}
+        onClick={handleStopStart}>
+        {isStarted ? 'Stop' : 'Start'}
+      </button>
 
       {/* TIMER */}
       <div className='time'>Time: {time}</div>
