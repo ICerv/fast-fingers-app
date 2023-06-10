b7c0d6109617459fc5e8f121587098098da688ec
Nataliia Hladii <natalia14101995@gmail.com>
2023-06-10 11:39:43 +0200

    Edit TrainingPage

--- src/index.jsx
+++ src/index.jsx
@@ -39,7 +39,7 @@ const router = createBrowserRouter([
         element: <TestsPage/>,
       },
       {
-        path: '/lesson/lesson1',
+        path: '/section/:sectionId/lesson/:lessonId',
         element: <TrainingPage/>,
       },
   
--- src/pages/LessonsPage/LessonsList/index.jsx
+++ src/pages/LessonsPage/LessonsList/index.jsx
@@ -3,14 +3,14 @@ import { useNavigate } from "react-router-dom";
 // import { lessons } from '../../exercisesSections';
 import './style.css'; 
 
-const LessonsList = ({lessons}) => {
+const LessonsList = ({lessons, sectionId}) => {
 
     const navigate = useNavigate()
 
     return (
         <div className='lessons-list'>
             {lessons.map((lesson) =>
-                <div className="lesson-card" key={lesson.id} onClick={() => navigate('/lesson/lesson1')}>
+                <div className="lesson-card" key={lesson.id} onClick={() => navigate(`/section/${sectionId}/lesson/${lesson.id}`)}>
                     <span className="lesson-number">Lekce {lesson.number}</span>
                     <div className="lesson-title">{lesson.title}</div>
                     <p className="lesson_intro">{lesson.amount}</p>
