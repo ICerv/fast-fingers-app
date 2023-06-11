942bf92054512ed5911f850a186d9dcf38afd6ea
Nataliia Hladii <natalia14101995@gmail.com>
2023-06-11 16:26:21 +0200

    Add scrollIntoView to the TextInput

--- src/pages/TrainingPage/TextInput/index.jsx
+++ src/pages/TrainingPage/TextInput/index.jsx
@@ -1,28 +1,48 @@
-import React, { useState, useEffect } from 'react';
-import './style.css';
+import React, { useRef, useEffect } from "react";
+import "./style.css";
 
 const TextInput = ({ targetText, inputText, exerciseMode }) => {
+  const ref = useRef();
+
+  useEffect(() => {
+    ref.current?.scrollIntoView({
+      behavior: "smooth",
+    });
+  }, [inputText]);
+
   const renderText = () => {
-    let isActive = true
+    let isActive = true;
     return Array.from(targetText).map((char, index) => {
       if (index < inputText.length) {
-        let className
+        let className;
         if (char === inputText[index]) {
-          className = 'letter right'
-          isActive = true
+          className = "letter right";
+          isActive = true;
         } else {
-          className = 'letter wrong'
-          isActive = false
+          className = "letter wrong";
+          isActive = false;
         }
-        return <span key={index} className={className }>{char}</span>
-      } if (index === inputText.length) {
-
-        if (!exerciseMode || (exerciseMode && isActive)  ) {
-          return <span key={index} className= 'letter active'>{char}</span>
+        return (
+          <span key={index} className={className}>
+            {char}
+          </span>
+        );
+      }
+      if (index === inputText.length) {
+        if (!exerciseMode || (exerciseMode && isActive)) {
+          return (
+            <span key={index} ref={ref} className="letter active">
+              {char}
+            </span>
+          );
         }
       }
-      
-      return <span key={index} className='letter'>{char}</span> 
+
+      return (
+        <span key={index} className="letter">
+          {char}
+        </span>
+      );
     });
   };
 
@@ -31,6 +51,6 @@ const TextInput = ({ targetText, inputText, exerciseMode }) => {
       <div>{renderText()}</div>
     </>
   );
-}
+};
 
 export default TextInput;
