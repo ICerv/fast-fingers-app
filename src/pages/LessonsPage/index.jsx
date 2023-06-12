fa2d8739adf9c9a95e4156965ccb13f8873b05bf
Nataliia Hladii <natalia14101995@gmail.com>
2023-06-12 16:09:05 +0200

    Add top button to the LessonsPage

--- src/pages/LessonsPage/index.jsx
+++ src/pages/LessonsPage/index.jsx
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { exercisesSections } from '../exercisesSections';
 import LessonsList from './LessonsList';
 import './style.css';
 
 const LessonsPage = () => {
   const [showLists, setShowLists] = useState({});
+  const [showTopBtn, setShowTopBtn] = useState(false);
 
   const toggleList = (sectionId) => {
     setShowLists((prevState) => ({
@@ -13,11 +14,28 @@ const LessonsPage = () => {
     }));
   };
 
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 400) {
+            setShowTopBtn(true);
+        } else {
+            setShowTopBtn(false);
+        }
+    });
+    }, []);
+
+    const goToTop = () => {
+      window.scrollTo({
+          top: 0,
+          behavior: 'smooth',
+      });
+    };
+
   return (
-    <>
+    <div className="page-container">
       {exercisesSections.map((section) => (
         <div className="lessons-container" key={section.id}>
-          <div className="lessons-section" onClick={() => toggleList(section.id)}>
+          <div className="lessons-section"  onClick={() => toggleList(section.id)}>
             <div className="section-container">
               <div className="section-info">
                 <img src={require(`${section.url}`)} className="section-image" />
@@ -32,7 +50,9 @@ const LessonsPage = () => {
           {showLists[section.id] && <LessonsList lessons={section.lessons} sectionId={section.id} />}
         </div>
       ))}
-    </>
+      {showTopBtn && 
+      (<img src={require('./img/icon-up.png')} className='icon-to-top' onClick={goToTop} />)}
+    </div>
   );
 };
 
