bb14b3978723b4566181c960bd00f31a8914981f
Inna Cervenkova <inna.cervenkova@gmail.com>
2023-06-14 18:12:59 +0200

    Change time format as mm:ss

--- src/components/Practice/Timer/index.jsx
+++ src/components/Practice/Timer/index.jsx
@@ -48,7 +48,13 @@ export const Timer = ({ onStart, onEnd, onReload, isStarted, targetText, current
     }
   };
 
-  const formattedTime = time < 10 ? `0${time}` : time;
+  const formatTime = (time) => {
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+    return `${minutes < 10 ? '0' : ''}${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+  };
+
+  const formattedTime = formatTime(time);
 
   return (
     <div className="timer-container">
