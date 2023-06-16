e66a511a1d7ca57268141222e73393dc935570e3
Inna Cervenkova <inna.cervenkova@gmail.com>
2023-06-16 16:29:39 +0200

    Replace original CSS styles with BEM methodology in HomePage

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -4,14 +4,7 @@ import './style.css';
 
 const HomePage = () => {
   const banner = 'Program pro trénink psaní na klávesnici';
-  const subHeadings = [
-    'Zlepši své psaní',
-    'Získej rychlost',
-    'Přesnost',
-    'Produktivitu',
-    'Psaní naslepo',
-    // 'Větší šancí na trhu práce',
-  ];
+  const subHeadings = ['Zlepši své psaní', 'Získej rychlost', 'Přesnost', 'Produktivitu'];
   const typingState = 'istyping';
   const deletingState = 'isdeleting';
 
@@ -21,24 +14,32 @@ const HomePage = () => {
   const [subHeading, setSubHeading] = useState('');
   const [isBannerFinished, setIsBannerFinished] = useState(false);
 
+  // Helper function for sleep functionality
   const sleep = (ms) => {
     return new Promise((resolve) => setTimeout(resolve, ms));
   };
 
   useEffect(() => {
     const timeout = setTimeout(() => {
+      // Typing the banner text until it's completed
       if (!isBannerFinished) {
         if (typing === typingState && bannerText !== banner) {
           setBannerText((originalText) => banner.slice(0, originalText.length + 1));
         } else if (bannerText === banner && typing === typingState) {
           setIsBannerFinished(true);
         }
+
+        // Once banner is typed, type the subheading
       } else if (typing === typingState && subHeading !== subHeadings[subHeadingIndex]) {
         setSubHeading((originalText) => subHeadings[subHeadingIndex].slice(0, originalText.length + 1));
+
+        // When subheading is completed, wait for 1 sec and switch to deleting
       } else if (subHeading === subHeadings[subHeadingIndex] && typing === typingState) {
         sleep(1000).then(() => {
           setTyping(deletingState);
         });
+
+        // Start deleting the subheading
       } else if (
         (subHeading === subHeadings[subHeadingIndex] && typing === deletingState) ||
         typing === deletingState
@@ -51,29 +52,33 @@ const HomePage = () => {
       }
     }, 100);
 
+    // Clear timeout when component unmounts
     return () => clearTimeout(timeout);
   }, [bannerText, subHeading, typing, isBannerFinished]);
 
   return (
-    <section className="homepage-section">
-      <div className="homepage-content">
-        <div className="homepage">
-          <h1 className="homepage-banner">
-            <pre className={isBannerFinished ? '' : 'blinking-cursor bannert-cursor'}>{bannerText}</pre>
+    <section className="homepage">
+      <div className="homepage__content">
+        <div className="homepage__wrapper">
+          <h1 className="homepage__banner">
+            <pre className={isBannerFinished ? 'homepage__banner--text' : 'homepage__banner--blinking-cursor'}>
+              {bannerText}
+            </pre>
           </h1>
-          <p className="homepage-subheading">
-            <span className={isBannerFinished ? 'subheading-text subheading-cursor blinking-cursor' : ''}>
+          <p className="homepage__subheading">
+            <span
+              className={isBannerFinished ? 'homepage__subheading--text homepage__subheading--blinking-cursor' : ''}
+            >
               {subHeading}
             </span>
           </p>
         </div>
 
-        <div className="homepage-image">
+        <div className="homepage__image">
           <img src={require('./img/laptop.png')} alt="Laptop image" />
         </div>
       </div>
     </section>
   );
 };
-
 export default HomePage;
