825081ad16d71f16072b7ebb20c08097871c3d1d
Inna Cervenkova <inna.cervenkova@gmail.com>
2023-06-18 13:44:09 +0200

    Replace original CSS styles with BEM methodology in CapsLockWarning

--- src/components/Practice/CapsLockWarning/index.jsx
+++ src/components/Practice/CapsLockWarning/index.jsx
@@ -2,29 +2,29 @@ import React, { useState, useEffect } from 'react';
 import './style.css';
 
 const CapsLockWarning = () => {
-    const [capsLockOn, setCapsLockOn] = useState(false);
-  
-    useEffect(() => {
-      const handleKeyDown = (event) => {
-        if (event.getModifierState('CapsLock')) {
-          setCapsLockOn(true);
-        } else {
-          setCapsLockOn(false);
-        }
-      };
-  
-      window.addEventListener('keydown', handleKeyDown);
-  
-      return () => {
-        window.removeEventListener('keydown', handleKeyDown);
-      };
-    }, []);
-  
-    if (capsLockOn) {
-      return <div className='capslock-warning'>Caps Lock je zapnutý!</div>;
-    } else {
-      return null;
-    }
-  };
-  
-  export default CapsLockWarning;
+  const [capsLockOn, setCapsLockOn] = useState(false);
+
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      if (event.getModifierState('CapsLock')) {
+        setCapsLockOn(true);
+      } else {
+        setCapsLockOn(false);
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []);
+
+  if (capsLockOn) {
+    return <div className="capslock-warning__container">Caps Lock je zapnutý!</div>;
+  } else {
+    return null;
+  }
+};
+
+export default CapsLockWarning;
